7fe7d06d5ecb40d6668f3e5269f7a167579f25cb
Fabian Silva <silvafabian74@yahoo.com>
2022-02-27 14:20:37 -0500

    add ceate function and export it

--- data/skill-db.js
+++ data/skill-db.js
@@ -38,7 +38,15 @@ const findById = (id, callback) =>{
   }
 }
 
+function create(skill, callback) {
+  skill._id - Date.now() % 1000000
+  skill.done = false
+  skills.push(skill)
+  return callback(null, skill)
+}
+
 export { 
 	find,
-  findById
+  findById,
+  create
 }
--- routes/skills.js
+++ routes/skills.js
@@ -7,6 +7,7 @@ const router = Router()
 router.get('/', skillsCtrl.index)
 router.get('/new', skillsCtrl.new)
 router.get('/:id', skillsCtrl.show)
+router.post('/', skillsCtrl.create)
 
 export {
   router
